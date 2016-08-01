532d713bfbf1567beb9a959741d1c991d7da62e4
yhhno <516061849@qq.com>
2016-08-01 17:33:12 +0800

    02实现基于Task的 异步模式 1.生成方法 1.1编译器生成 1.2手动生成 1.3混合生成 2.工作负荷 2.1计算受限 2.2I/O限制 2.3混合计算受限和I/O限制的任务

---
+++ 基于Task的异步模式/02实现基于Task的异步模式/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02实现基于Task的异步模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
